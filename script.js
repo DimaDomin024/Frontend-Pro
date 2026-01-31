c817b4fa3e27dbf49747f9c70fd34fe96bb478b3
Rai11ox <ddemin024@gmail.com>
2026-01-31 17:24:10 +0200

    task4.1

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-let newNumber = prompt("Enter u naumber with 5 digits");
-
-let d1 = Math.floor(newNumber / 10000)
-let d2 = Math.floor(newNumber / 1000) % 10
-let d3 = Math.floor(newNumber / 100) % 10
-let d4 = Math.floor(newNumber / 10) % 10
-let d5 = newNumber % 10 
-
-let result = `${d1} ${d2} ${d3} ${d4} ${d5}`
-
-console.log(result);
+let name = prompt("Enter your name: ");
+alert(`Hello, ${name}! How are you?`);
