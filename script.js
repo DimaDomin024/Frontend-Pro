0d9a00472ee31884a00d2765fe3a591b54089fc8
Rai11ox <ddemin024@gmail.com>
2026-01-31 16:48:02 +0200

    task3.1

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert('Are u ready?');
+console.log(typeof string); 
+console.log(typeof 'string'); 
+console.log(typeof 45); 
+console.log(typeof true); 
+console.log(typeof []);
+console.log(typeof 123n);
+console.log(typeof function() {});
