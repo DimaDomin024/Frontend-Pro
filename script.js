b641902affc5d3306889f2445819f99499239e95
Rai11ox <ddemin024@gmail.com>
2026-02-03 12:35:14 +0200

    task4.1(without watch video lesson)

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-let name = prompt("Enter your name: ");
-alert(`Hello, ${name}! How are you?`);
+let number = prompt("Enter your number: ");
+let a = number / 100 ;
+let b = number % 100;
+let c = number % 10; 
+if (a == b && b == c){alert ("true")}
+else {alert ("false")}
