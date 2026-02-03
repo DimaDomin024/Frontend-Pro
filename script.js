415bedd3e57dfd0843eb2da4110b3a9d61cdb4f1
Rai11ox <ddemin024@gmail.com>
2026-02-03 15:53:50 +0200

    task4.2

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-let number = prompt("Enter your number: ");
-let a = number / 100 ;
-let b = number % 100;
-let c = number % 10; 
-if (a == b && b == c){alert ("true")}
-else {alert ("false")}
+let number = +prompt("Enter your number:");
+
+if (!number || isNaN(number)) {
+  console.log("Невалідне значення");
+} else {
+  let a = Math.floor(number / 100);
+  let b = Math.floor((number % 100) / 10);
+  let c = number % 10;
+
+  if (a === b && b === c) {
+    console.log("Числа однакові");
+  } else if (a === b || a === c || b === c) {
+    console.log("Є однакові числа, але не всі");
+  } else {
+    console.log("Числа всі різні");
+  }
+
+}
 
