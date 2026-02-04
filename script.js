2ec16725bb44781cf5d9d92a5b2aa15bdd9acc96
Rai11ox <ddemin024@gmail.com>
2026-02-04 09:02:16 +0200

    task5.4

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-let number = +prompt("Enter a number:");
-for (let i = 1 ; i <= 100 ; i ++){
-    if (i * i < number){
-        console.log(i);
+// let number = Math.floor(prompt("Enter a number:"));
+
+// let i = 2;
+
+// if (number <= 1) {
+//     console.log("Просте");
+// } else {
+//     while (i < number) {
+//         if (number % i === 0) {
+//             console.log("складне")
+//             break;
+//         }
+//         i++;
+        
+//     }
+    
+// }
+// console.log("Просте");
+//З'ясувати, чи просто воно (простим називається число, більше 1, що не має інших дільників, крім 1 і себе).
+
+let number = Math.floor(prompt("Enter a number:"));
+
+let config = true ;
+
+for(let i = 2 ; i < number ; i ++){
+    if(number % i === 0 ){
+        config = false;
+        break;
     }
+    
 }
+if (config === true){
+        console.log("Просте");
+    }else {
+        console.log("Складне");
+    }
+
