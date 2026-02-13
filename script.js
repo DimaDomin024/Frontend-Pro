f4e2520d42555552fab907f0ba208bfac6a25f14
Rai11ox <ddemin024@gmail.com>
2026-02-13 09:22:36 +0200

    task6.2

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const user =  {
-    name: prompt("Enter your name:"),
-    age: +prompt("Enter your age:"),
-    city: prompt("Enter your city:"),
-    Number  : +prompt("Enter your phone number:")
-}
 
-for (const key of Object.values(user)){
-    console.log(key);
+const number = [];
+while (true) {
+    const input = prompt("Enter a number (or type 'stop' to finish):");
+    if (input === null || input === "stop" || input === "") {
+        break;
+    }
+    else {
+        if (input % 2 === 0) {
+            number.push(input);
+        
+        }
+    }   
 }
+console.log(number);
+
