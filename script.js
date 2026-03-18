9ec84f23b840584a7a4eb05662bbb0d7ac1e7462
Rai11ox <ddemin024@gmail.com>
2026-03-18 19:26:58 +0200

    task7.1

--- script.js
+++ script.js
@@ -1,68 +1,13 @@
-const book = {
-    contacts : [ 
-        {
-        
-            name: "John",
-            phone: "1234567890",
-            email: "john.doe@example.com"   
+let first = prompt("Enter u first string")
+let second = prompt("Second")
 
-        },
-        {
-        
-            name: "Piter",
-            phone: "06656565665",
-            email: "peter.parker@example.com"   
+let remove = function(first,second){
+    let result = first;
 
-        },
-        {
-        
-            name: "andrew",
-            phone: "1234567890",
-            email: "andrew.smith@example.com"   
-
-        },
-    ],
-}
-
-while (true) {
-    const input = prompt("add or search contact? (add/search/exit)");
-    if (input === null || input.toLowerCase() === "exit" || !input) {
-        break;
+    for(let i = 0; i < second.length; i++ ){
+        let char = second[i];
+        result = result.replaceAll(char, '');
     }
-
-    if (input.toLowerCase() === "add") {
-        const user = prompt("Enter contact name: name/phone/email");
-        const separ = user.split("/");
-
-        const newUser = {
-            name : separ[0],
-            phone : separ[1],
-            email : separ[2] 
-        };
-        book.contacts.push(newUser);
-            console.log(
-            Object.values(book.contacts)
-            );
-    }
-    if (input.toLowerCase() === "search"){
-        alert("searching contact ......");
-        const search = prompt("Enter contact name to search: name or phone or email");
-
-        let found = false;
-        for (let i = 0 ; i < book.contacts.length; i++) {
-            if (book.contacts[i].name === search || book.contacts[i].phone === search || book.contacts[i].email === search) {
-                alert(`Contact found: ${book.contacts[i].name}, ${book.contacts[i].phone}, ${book.contacts[i].email}`);
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            alert("Contact not found");
-        }
-        console.log(search);
-        }
-        
+    return result;
 }
-
-
-
+console.log(remove(first,second))
