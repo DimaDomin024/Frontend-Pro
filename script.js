657c948c824a1044a35a511273bf58bcad0fc147
Rai11ox <ddemin024@gmail.com>
2026-03-18 19:35:03 +0200

    task8.1

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
-
-// Наприклад:
-
-const array = [1, 3, 4, 6, 2, 5, 7];
-
-function removeElement(array, item){
-    if (item > 0 && item < array.length){
-        array.splice(item , 1)
+function outer(result){
+    let x = result;
+    
+    function first(amount){
+        x += amount;
+        console.log("first = ", x );
     }
+    return first;
 }
 
+let finish = outer(0)
 
-removeElement(array,1);
-
-console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
+finish(4);
+finish(6);
+finish(10);
+finish(7);
