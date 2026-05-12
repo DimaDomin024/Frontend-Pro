cddd6a66d90c5a6f92d39de1b76884724e133397
Rai11ox <ddemin024@gmail.com>
2026-05-12 09:16:07 +0300

    task11.3

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const myButton = document.querySelector("#myButton")
-const myText = document.querySelector("#myText")
+const img = document.createElement("img");
+img.src = "pict/" + (Math.floor(Math.random() * 9) + 1) + ".jpg";
 
+document.body.append(img);
 
-
-function changeColor(){
-    myText.classList.toggle("active");
-}
-
-
-myButton.addEventListener("click",changeColor);
+console.log(img);
