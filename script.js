28ac02d2496eb7be9d9dd1131a3bc13266477632
Rai11ox <ddemin024@gmail.com>
2026-05-12 08:40:31 +0300

    task10.1

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-let step = 0;
+let company = {
+    
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
+    
+    development: {
 
+        web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800}],
 
-let ladder = {
-  up: function () {
-    step ++ ;
-    return this;
-  },
-  down: function () {
-    step --;
-    return this;
-  },
-  showStep: function () { 
-    return console.log(step);
-  }
+        internals: [{name: 'Jack', salary: 1300}]
+    }
 };
 
-ladder.up().up().down().showStep();
+
+function sumSalary(obj){
+    if (Array.isArray(obj)) {
+    let sum = 0;
+    for (let i = 0; i < obj.length; i++) {
+      sum += obj[i].salary ;
+    }
+    return sum;
+    }    
+    let sum = 0;
+    for(let key in obj) {
+        sum += sumSalary(obj[key]);
+    }
+
+    return sum;
+
+}
+
+console.log(
+    sumSalary(company)
+);
