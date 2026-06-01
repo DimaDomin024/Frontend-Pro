3dbc576c4800dab89bf9aeaf714e8184c7a06edd
Rai11ox <ddemin024@gmail.com>
2026-06-01 19:02:02 +0300

    task12.2

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-const tasks = document.querySelector("#tasks")
-const actions = ["Task 1","Task 2","Task 3"];
-const input = document.querySelector("#addTask")
-const addButton = document.querySelector("#addButton")
+const container = document.querySelector("#fContainer");
 
-function createTask(newInput){
-    const item = document.createElement("li");
-    const itemButton = document.createElement("button");
-    item.classList.add("action");
-    itemButton.classList.add("itemButton");
-    item.textContent = newInput;
-    itemButton.textContent = "Delete";
-    item.append(itemButton);
-    tasks.append(item);
-    itemButton.addEventListener("click", e => {
-        item.remove();
+    container.addEventListener("click", (e) => {
+        if (e.target.tagName === "BUTTON") {
+            alert(`Натиснуто: ${e.target.textContent}`);
+        }
     });
-}
-actions.forEach(action => createTask(action)) 
-
-addButton.addEventListener("click", e => {
-    createTask(input.value);
-    input.value = "";
-    e.stopPropagation();
-})
